d065a195199c2f68fd48659620bcd15337a0f449
Dakota Parker <dakotacolorado2@gmail.com>
2023-09-26 22:32:17 -0700

    update vector functionality

--- src/chinese_checkers_game/vector.py
+++ src/chinese_checkers_game/vector.py
@@ -6,4 +6,10 @@ class Vector:
         self.j = j
 
     def __eq__(self, other: 'Vector') -> bool:
+        if not isinstance(other, Vector):
+            return False
+
         return self.i == other.i and self.j == other.j
+
+    def __hash__(self):
+        return hash((self.i, self.j))
--- tests/chinese_checkers_game/test_vector.py
+++ tests/chinese_checkers_game/test_vector.py
@@ -1,9 +1,9 @@
-import unittest
+from unittest import TestCase
 
-from chinese_checkers_game.vector import Vector
+from src.chinese_checkers_game.vector import Vector
 
 
-class TestVector(unittest.TestCase):
+class TestVector(TestCase):
 
     def test_initialization(self):
         v = Vector(1, 2)
@@ -23,3 +23,23 @@ class TestVector(unittest.TestCase):
     def test_equality_with_non_vector(self):
         v = Vector(1, 2)
         self.assertFalse(v == "not a vector")
+
+    def test_hash_for_equal_vectors(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(1, 2)
+        self.assertEqual(hash(v1), hash(v2))
+
+    def test_vector_in_set(self):
+        v1 = Vector(1, 2)
+        v2 = Vector(1, 2)  # Equal to v1
+        v3 = Vector(3, 4)  # Different from v1
+
+        s = {v1}
+
+        # Since v1 and v2 are equal, adding v2 shouldn't increase the set size
+        s.add(v2)
+        self.assertEqual(len(s), 1)
+
+        # Adding v3 should increase the set size
+        s.add(v3)
+        self.assertEqual(len(s), 2)
