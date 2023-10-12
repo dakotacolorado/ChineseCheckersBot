6cc91d378b37c9af0e6f9005118bb203754ec2c8
Dakota Parker <dakotacolorado2@gmail.com>
2023-10-12 01:05:44 -0700

    add player IDs

--- src/chinese_checkers/game/player.py
+++ src/chinese_checkers/game/player.py
@@ -9,13 +9,22 @@ class Player:
     def __init__(
             self,
             positions: List[Position],
-            target_positions: List[Position]
+            target_positions: List[Position],
+            player_id: str
     ):
         self.positions = positions
         self.target_positions = target_positions
+        self.player_id = player_id
 
     def apply_move(self, move: Move):
-        return Player([
-            move.apply() if position == move.position else position
-            for position in self.positions
-        ])
+        return Player(
+            [
+                move.apply() if position == move.position else position
+                for position in self.positions
+            ],
+            self.target_positions,
+            self.player_id
+        )
+
+    def __eq__(self, other: 'Player') -> bool:
+        return self.player_id == other.player_id
