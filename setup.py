10c7c1792dea937e14098bc8d2d4884b1e63cb18
Dakota Parker <dakotacolorado2@gmail.com>
2023-10-14 13:06:01 -0700

    update package settings

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="chinese_checkers",
     version="0.1",
+    author="Dakota James Parker",
+    description="A Chinese Checkers game implementation.",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     extras_require={
-        "test": ["pytest", "pytest-cov"],  # add your testing dependencies here
+        "test": ["pytest", "pytest-cov"],
     },
 )
