5da7734ed2f82287f42aacf8995735b2ac72bba8
Dakota Parker <dakotacolorado2@gmail.com>
2023-10-15 20:52:34 -0700

    add model interface tests

---
+++ tests/chinese_checkers/model/test_IModel.py
@@ -0,0 +1,25 @@
+from src.chinese_checkers.model.IModel import IModel
+from src.chinese_checkers.game.ChineseCheckersGame import ChineseCheckersGame
+from src.chinese_checkers.game.Player import Player
+from src.chinese_checkers.game.Position import Position
+from unittest import TestCase
+
+
+class BasicModel(IModel):
+    def _chose_next_move(self, current_player, other_players, moves):
+        """ Chose the first move in the list of moves. """
+        return moves[0]
+
+
+class TestIModel(TestCase):
+    def test_make_move(self):
+        game = ChineseCheckersGame.start_game()
+        current_moves = game.get_next_moves()
+
+        expected_game = ChineseCheckersGame.start_game()
+        expected_game = expected_game.apply_move(current_moves[0])
+
+        model = BasicModel(game)
+        game = model.make_move()
+
+        self.assertEqual(expected_game, game)
