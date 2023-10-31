bbfc2f3bf040e497683e4720edcd366894b1f4ac
Dakota Parker <dakotacolorado2@gmail.com>
2023-10-31 03:43:31 -0700

    [REFACTOR] enhance sim data with errors and extracted functions

--- src/chinese_checkers/simulation/SimulationData.py
+++ src/chinese_checkers/simulation/SimulationData.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
-from typing import List
-
+from typing import List, Type, Optional
 import numpy as np
-
 from ..game.Move import Move
 from ..game.Position import Position
 
@@ -10,35 +8,49 @@ from ..game.Position import Position
 @dataclass(frozen=True)
 class SimulationData:
     """Data related to player positions in the game."""
+
     player_ids: List[str]
     player_start_positions: List[List[Position]]
     player_target_positions: List[List[Position]]
     historical_moves: List[Move]
 
-    def to_storable(self: "SimulationData") -> dict:
-        """Converts the object to a format suitable for h5py storage."""
+    def to_storable(self) -> dict:
+        """
+        Converts the object to a format suitable for h5py storage.
+
+        Notes:
+            The player_ids are converted to byte strings to be compatible with h5py.
+        """
         return {
-            "player_ids": np.array(self.player_ids, dtype="S"),  # Convert to byte strings for h5py
-            "player_start_positions": np.array(
-                [[pos.to_tuple() for pos in sublist] for sublist in self.player_start_positions]
-            ),
-            "player_target_positions": np.array(
-                [[pos.to_tuple() for pos in sublist] for sublist in self.player_target_positions]
-            ),
+            "player_ids": self._player_ids_to_bytes(self.player_ids),
+            "player_start_positions": self._nested_list_to_array(self.player_start_positions),
+            "player_target_positions": self._nested_list_to_array(self.player_target_positions),
             "historical_moves": np.array([move.to_tuple() for move in self.historical_moves])
         }
 
     @staticmethod
-    def from_storable(data: dict) -> "SimulationData":
-        """Creates a GamePositions object from stored h5py data."""
-        player_ids = [pid.decode() for pid in data["player_ids"]]
-        player_start_positions = [
-            [Position.from_tuple(tuple(pos)) for pos in sublist] for sublist in data["player_start_positions"].tolist()
-        ]
-        player_target_positions = [
-            [Position.from_tuple(tuple(pos)) for pos in sublist] for sublist in data["player_target_positions"].tolist()
-        ]
-        historical_moves = [Move.from_tuple(tuple(move)) for move in data["historical_moves"].tolist()]
+    def _player_ids_to_bytes(player_ids) -> np.ndarray:
+        return np.array(player_ids, dtype="S")
+
+    @staticmethod
+    def _nested_list_to_array(data: List[List[Position]]) -> np.ndarray:
+        return np.array([[pos.to_tuple() for pos in sublist] for sublist in data])
+
+    @staticmethod
+    def from_storable(data: dict) -> 'SimulationData':
+        """
+        Creates a GamePositions object from stored h5py data.
+
+        Ensures that the required keys exist in the data dictionary to safeguard against potential errors.
+        """
+        if not all(key in data for key in
+                   ["player_ids", "player_start_positions", "player_target_positions", "historical_moves"]):
+            raise ValueError("The provided data dictionary is missing required keys.")
+
+        player_ids = list(map(lambda x: x.decode(), data["player_ids"]))
+        player_start_positions = SimulationData._convert_to_positions(data["player_start_positions"])
+        player_target_positions = SimulationData._convert_to_positions(data["player_target_positions"])
+        historical_moves = list(map(lambda move: Move.from_tuple(tuple(move)), data["historical_moves"]))
 
         return SimulationData(
             player_ids=player_ids,
@@ -46,3 +58,7 @@ class SimulationData:
             player_target_positions=player_target_positions,
             historical_moves=historical_moves
         )
+
+    @staticmethod
+    def _convert_to_positions(data: np.ndarray) -> List[List[Position]]:
+        return [[Position.from_tuple(tuple(pos)) for pos in sublist] for sublist in data]
--- tests/chinese_checkers/simulation/test_SimulationData.py
+++ tests/chinese_checkers/simulation/test_SimulationData.py
@@ -18,13 +18,26 @@ class TestSimulationData(unittest.TestCase):
         )
 
     def test_to_storable(self):
+        """Test if the to_storable method returns appropriate numpy arrays."""
         storable = SimulationData.to_storable(self.game_positions)
         self.assertTrue(isinstance(storable["player_ids"], np.ndarray))
         self.assertTrue(isinstance(storable["player_start_positions"], np.ndarray))
         self.assertTrue(isinstance(storable["player_target_positions"], np.ndarray))
         self.assertTrue(isinstance(storable["historical_moves"], np.ndarray))
 
+        self.assertEqual(storable["player_ids"].tolist(), [b"P1", b"P2"])
+
     def test_from_storable(self):
+        """Test if the from_storable method restores the original SimulationData object."""
         storable = SimulationData.to_storable(self.game_positions)
         restored = SimulationData.from_storable(storable)
         self.assertEqual(restored, self.game_positions)
+
+        self.assertEqual(restored.player_ids, self.game_positions.player_ids)
+
+    def test_from_storable_with_missing_key(self):
+        """Test the behavior when a required key is missing."""
+        storable = SimulationData.to_storable(self.game_positions)
+        del storable["player_ids"]
+        with self.assertRaises(ValueError):
+            SimulationData.from_storable(storable)
