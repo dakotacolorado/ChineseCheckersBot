9d7a253a93075de2425f1794a50d076a6069ac1a
Dakota Parker <46725372+dakotacolorado@users.noreply.github.com>
2024-11-10 19:07:23 -0800

    Remove generator from experience.

--- src/chinese_checkers/reinforcement/experience/Experience.py
+++ src/chinese_checkers/reinforcement/experience/Experience.py
@@ -8,7 +8,7 @@ from ..encode import SpatialMoveMetricsEncoder, RewardEncoder, SpatialBoardMetri
 from ...catalog import IDataMetadata
 from ...game import ChineseCheckersGame
 from ...simulation import GameSimulation, SimulationMetadata
-
+from ..encode.RewardEncoderV2 import RewardEncoderV2
 
 @dataclass(frozen=True)
 class Experience(IDataMetadata[ExperienceData, ExperienceMetadata]):
@@ -19,53 +19,3 @@ class Experience(IDataMetadata[ExperienceData, ExperienceMetadata]):
     @staticmethod
     def from_data_metadata(data: ExperienceData, metadata: ExperienceMetadata) -> 'Experience':
         return Experience(data, metadata)
-
-    @staticmethod
-    def generate_experiences_from_simulation(
-            simulation: GameSimulation,
-            generator_name: str
-    ) -> List["Experience"]:
-
-        if generator_name == "v0.0.2":
-            return Experience._generate_experiences_v0_0_2(simulation, generator_name)
-        else:
-            raise ValueError(f"Unsupported encoder name '{generator_name}'")
-
-    @staticmethod
-    def _generate_experiences_v0_0_2(
-            simulation: GameSimulation,
-            generator_name: str
-    ) -> List["Experience"]:
-        move_encoder = SpatialMoveMetricsEncoder()
-        reward_encoder = RewardEncoder()
-        board_state_encoder = SpatialBoardMetricsEncoder(simulation.metadata.board_size)
-
-        rewards = reward_encoder.encode(simulation)
-        game_sequence: List[ChineseCheckersGame] = simulation._to_game_sequence()
-
-        experiences = []
-        for reward, move, current_game_state in zip(
-                rewards,
-                simulation.data.historical_moves,
-                game_sequence
-            ):
-            next_game_state = current_game_state.apply_move(move)
-            is_terminal = torch.tensor(next_game_state.is_game_won(), dtype=torch.float32)  # Encoded as tensor
-
-            experience = Experience(
-                ExperienceData(
-                    state=board_state_encoder.encode(current_game_state),
-                    action=move_encoder.encode(move, current_game_state),
-                    reward=reward,
-                    next_state=board_state_encoder.encode(next_game_state),
-                    done=is_terminal
-                ),
-                ExperienceMetadata.from_simulation_metadata(
-                    simulation.metadata,
-                    generator_name=generator_name,
-                    current_player= current_game_state.get_current_player().player_id,
-                )
-            )
-            experiences.append(experience)
-
-        return experiences
--- src/chinese_checkers/reinforcement/experience/__init__.py
+++ src/chinese_checkers/reinforcement/experience/__init__.py
@@ -2,10 +2,12 @@ from .Experience import Experience
 from .ExperienceCatalog import ExperienceCatalog
 from .ExperienceData import ExperienceData
 from .ExperienceMetadata import ExperienceMetadata
+from .ExperienceAnalysis import ExperienceAnalysis
 
 __all__ = [
     'Experience',
     'ExperienceCatalog',
     'ExperienceData',
-    'ExperienceMetadata'
+    'ExperienceMetadata',
+    'ExperienceAnalysis'
 ]
